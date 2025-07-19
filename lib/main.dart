0565beed00a4565d535ed357f5416a773a54b152
Khalid Durrani <khaliddurrani113@gmail.com>
2025-07-19 22:17:57 +0430

    Providers Separated

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,12 @@ import 'package:yelpax/config/localization/l10n/l10n.dart';
 import 'package:yelpax/config/localization/locale_provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:yelpax/config/themes/theme_provider.dart';
+import 'package:yelpax/providers/providers.dart';
+import 'config/themes/theme_mode_type.dart';
 import 'generated/app_localizations.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => LocaleProvider(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(MultiProvider(providers: appProviders, child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = Provider.of<LocaleProvider>(context);
     final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
       locale: provider.locale,
       supportedLocales: L10n.all,
@@ -35,23 +33,21 @@ class MyApp extends StatelessWidget {
       ],
       title: 'Flutter Demo',
       theme: themeProvider.themeData,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        themeProvider: themeProvider,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.themeProvider,
+  });
+  final ThemeProvider themeProvider;
   final String title;
 
   @override
@@ -63,52 +59,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
+            DropdownButton<ThemeModeType>(
+              value: widget.themeProvider.currentTheme,
+              items: ThemeModeType.values.map((type) {
+                return DropdownMenuItem(
+                  value: type,
+                  child: Text(type.toString().split('.').last),
+                );
+              }).toList(),
+              onChanged: (type) {
+                if (type != null) {
+                  widget.themeProvider.setTheme(type);
+                }
+              },
+            ),
+
             Text(AppLocalizations.of(context)!.icrement),
             Text(
               '$_counter',
