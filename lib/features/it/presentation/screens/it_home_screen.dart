0ffea76cb38a15c067a90165a1a9181ee0d169f6
Khalid-Durrani-2024 <khaliddurrani113@gmail.com>
2025-09-09 23:54:51 +0430

    hot/fix IT services

--- lib/features/it/presentation/screens/it_home_screen.dart
+++ lib/features/it/presentation/screens/it_home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:yelpax/features/it/presentation/widgets/it_search_widget.dart';
 
 import '../../../../app/presentation/shell/widgets/custom_bottom_nav.dart';
+import '../../../../config/routes/router.dart';
 import '../../../../config/themes/theme_mode_type.dart';
 import '../../../../config/themes/theme_provider.dart';
 import '../../../../core/constants/height.dart';
@@ -15,7 +17,6 @@ class ItHomeScreen extends StatefulWidget {
 }
 
 class _ItHomeScreenState extends State<ItHomeScreen> {
-
   void _initializeData() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final theme = Provider.of<ThemeProvider>(context, listen: false);
@@ -25,18 +26,17 @@ class _ItHomeScreenState extends State<ItHomeScreen> {
       // );
 
       theme.setTheme(ThemeModeType.it);
- //     controller.getCategories();
+      //     controller.getCategories();
     });
   }
 
-@override
+  @override
   void initState() {
-   _initializeData();
+    _initializeData();
     // TODO: implement initState
     super.initState();
   }
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,10 +46,39 @@ class _ItHomeScreenState extends State<ItHomeScreen> {
       ),
       drawer: Drawer(),
       backgroundColor: Colors.white,
-      body: Center(child: Text('IT Services')),
+      body: _buildBody(),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: 0,
         onTap: (value) => print(value),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      child: RefreshIndicator.adaptive(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                BackButton(
+                  onPressed: () =>
+                      Navigator.pushReplacementNamed(context, AppRouter.home),
+                ),
+                Text(
+                  'Home Services',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                ItSearchWidget(),
+              ],
+            ),
+          ],
+        ),
+        onRefresh: () async {
+          print('Refreshed');
+          return;
+        },
       ),
     );
   }
---
+++ lib/features/it/presentation/widgets/it_search_widget.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yelpax/features/it/presentation/controllers/it_home_controller.dart';
+import '../../../../shared/widgets/custom_button.dart';
+import '../../../../shared/widgets/custom_shimmer.dart';
+import '../../../../shared/widgets/custom_input.dart';
+
+class ItSearchWidget extends StatelessWidget {
+  const ItSearchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ItHomeController(),
+      child: const _searchProBody(),
+    );
+  }
+}
+
+class _searchProBody extends StatefulWidget {
+  const _searchProBody({super.key});
+
+  @override
+  State<_searchProBody> createState() => __searchProBodyState();
+}
+
+class __searchProBodyState extends State<_searchProBody> {
+  late TextEditingController _searchController;
+  late TextEditingController _zipController;
+  final formKey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    _searchController = TextEditingController();
+    _zipController = TextEditingController();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    _zipController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  void onCompleteCategory(String name) {
+    _searchController.text = name;
+
+    var _searchProController = Provider.of<ItHomeController>(
+      context,
+      listen: false,
+    );
+    _searchProController.clearResult();
+  }
+
+  void onClear() {
+    var _searchProController = Provider.of<ItHomeController>(
+      context,
+      listen: false,
+    );
+    _searchProController.clearResult();
+    _searchController.clear();
+  }
+
+  // void onChanged(String value) {
+  //   var _searchProController = Provider.of<ItHomeController>(
+  //     context,
+  //     listen: false,
+  //   );
+  //   _searchProController.search(value);
+  // }
+
+  void onSearch() {
+    var controller = Provider.of<ItHomeController>(
+      context,
+      listen: false,
+    );
+    if (formKey.currentState!.validate()) {
+      controller.openCategory({'name': _searchController.text}, context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CustomInput(
+          //  onChanged: (value) => onChanged(value),
+            validator: (p0) {
+              if (p0 == null || p0.isEmpty) {
+                return 'Enter Service';
+              }
+              return null;
+            },
+            hint: 'Which Service do you need',
+            icon: Icons.search,
+            controller: _searchController,
+            suffixIcon: GestureDetector(
+              onTap: onClear,
+              child: Icon(Icons.clear),
+            ),
+          ),
+          SizedBox(height: 10),
+          CustomInput(
+            validator: (p0) {
+              if (p0 == null || p0.isEmpty) return 'Enter Zip Code';
+
+              return null;
+            },
+            hint: 'Zip Code',
+            icon: Icons.location_on_outlined,
+            controller: _zipController,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                _zipController.clear();
+              },
+              child: Icon(Icons.clear),
+            ),
+          ),
+          SizedBox(height: 20),
+          CustomButton(
+            type: CustomButtonType.primary,
+
+            text: 'Search',
+
+            onPressed: onSearch,
+          ),
+          SizedBox(height: 10),
+          Consumer<ItHomeController>(
+            builder: (context, value, child) {
+              if (value.isLoading) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: CustomShimmer(
+                    layoutType: ShimmerLayoutType.list,
+                    itemCount: 2,
+                  ),
+                );
+              }
+              if (value.errorMessage.isNotEmpty) {
+                return Text(value.errorMessage);
+              }
+              if (value.itServices.isEmpty) {
+                return SizedBox.shrink();
+              }
+              return Container(
+                height: 150,
+                width: double.infinity,
+                child: ListView.builder(
+                  itemCount: value.itServices.length,
+                  itemBuilder: (context, index) =>
+                      _buildSearchFieldData(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchFieldData() {
+    return Center(
+      child: ListTile(
+      //  onTap: () => onCompleteCategory(pro.name),
+        leading: CircleAvatar(child: Text('1')),
+        title: Text('Software Development'),
+      ),
+    );
+  }
+}
